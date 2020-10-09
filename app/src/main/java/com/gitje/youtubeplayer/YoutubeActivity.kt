2b2d909118c82b58e724247d36e6a97ff8152e81
Gitjeshub <38493098+Gitjeshub@users.noreply.github.com>
2020-10-09 15:52:55 +0200

    Player Initialised
    Player now playing a video

--- app/src/main/java/com/gitje/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/gitje/youtubeplayer/YoutubeActivity.kt
@@ -1,26 +1,61 @@
 package com.gitje.youtubeplayer
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+
+const val YOUTUBE_VIDEO_ID = "-1qju6V1jLM"
+const val YOUTUBE_PLAYLIST_ID = "PLP6jQzrtQ719EDIDVLSpOu5q5-qy74ntT"
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    private val TAG = "YoutubeActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+        //setContentView(R.layout.activity_youtube)
+        //val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layout.addView(playerView)
+
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY), this)
     }
 
     override fun onInitializationSuccess(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubePlayer?,
-        p2: Boolean) {
-        TODO("Not yet implemented")
+        provider: YouTubePlayer.Provider?,
+        player: YouTubePlayer?,
+        wasRestored: Boolean
+    ) {
+        Log.d(TAG, "OnInitialization Success: provider is ${provider?.javaClass}")
+        Log.d(TAG, "OnInitialization Success: player is ${player?.javaClass}")
+        Toast.makeText(this, "Initialised YoutubePlayer successfully", Toast.LENGTH_SHORT).show()
+
+        if (!wasRestored)
+            player?.cueVideo(YOUTUBE_VIDEO_ID)
     }
 
     override fun onInitializationFailure(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubeInitializationResult?) {
-        TODO("Not yet implemented")
+        provider: YouTubePlayer.Provider?,
+        youTubeInitializationResult: YouTubeInitializationResult?
+    ) {
+        val REQUEST_CODE = 0
+        if (youTubeInitializationResult?.isUserRecoverableError == true) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show()
+        } else {
+            val errorMessage =
+                "There was an error initializing the YoutubePlayer ($youTubeInitializationResult)"
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
